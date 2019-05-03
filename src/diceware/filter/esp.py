ca381e00c7e9204ae6a3eed08cb529d4c21f2065
Oriol Arcas <oriol@starflownetworks.com>
2019-05-03 13:30:27 +0200

    GPL license in sources
    Signed-off-by: Oriol Arcas <oriol@starflownetworks.com>

--- src/diceware/filter/esp.py
+++ src/diceware/filter/esp.py
@@ -1,7 +1,20 @@
 '''
-Created on Apr 25, 2019
+Copyright (C) 2019 Oriol Arcas
 
-@author: oriol
+This file is part of DicewareGen.
+
+DicewareGen is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DicewareGen is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DicewareGen.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 import sys
