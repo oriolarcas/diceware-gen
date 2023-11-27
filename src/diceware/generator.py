a0346af1c50048f18383a990b2881b4d1221cb4a
Oriol Arcas <oriol.arcas@gmail.com>
2023-11-27 23:45:38 +0100

    Move generator to top level

--- src/diceware/generator.py
+++ src/diceware/generator.py
@@ -17,15 +17,15 @@ You should have received a copy of the GNU General Public License
 along with DicewareGen.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-import sys
 from time import time
 from difflib import get_close_matches
+from os import path
 
 class IndexGen:
     def __init__(self, sym, digits):
         self.sym = sym
         self.counters = [0] * digits
-    
+
     def __iter__(self):
         return self
 
@@ -44,40 +44,43 @@ class IndexGen:
         return current
 
 class DicewareGenerator:
-    def __init__(self):
+    def __init__(self, words_file, symbols_file=None, banned_file=None):
+        self.words_file = words_file
         self.translation = {}
-        if len(sys.argv) == 4:
-            with open(sys.argv[3]) as f:
-                for l in f.readlines():
-                    cs = l.decode("utf-8").strip().split(" ")
-                    self.translation[cs[0]] = cs[1:]
+        with open(symbols_file) as f:
+            for l in f.readlines():
+                cs = l.decode("utf-8").strip().split(" ")
+                self.translation[cs[0]] = cs[1:]
 
         self.banned_list = set()
-        if len(sys.argv) >= 3:
-            with open(sys.argv[2]) as f:
-                for l in f.readlines():
-                    w = l.strip()
-                    if len(w) == 0 or w[0] == "#":
-                        continue
-                    w = self.translate_word(w.decode("utf-8"), self.translation)
-                    self.banned_list.add(w)
-    
+        with open(banned_file) as f:
+            for l in f.readlines():
+                w = l.strip()
+                if len(w) == 0 or w[0] == "#":
+                    continue
+                w = self.translate_word(w.decode("utf-8"), self.translation)
+                self.banned_list.add(w)
+
+    @staticmethod
+    def _replace_file_in_path(file_path, new_file):
+        return path.join(path.dirname(file_path), new_file)
+
     def translate_word(self, w, subs):
         for newc, oldcs in subs.items():
             for oldc in oldcs:
                 w = w.replace(oldc, newc)
         return w
-    
-    def read_words(self, filename):
+
+    def read_words(self):
         raise NotImplementedError
-    
+
     def gen_diceware_list(self):
         last_time = time()
         last_index = 1
-        
+
         accepted_words = []
         index = 0
-        for w_original in self.read_words(sys.argv[1]):
+        for w_original in self.read_words():
             index += 1
             w = self.translate_word(w_original, self.translation)
             if len(w) <= 3:
@@ -96,7 +99,7 @@ class DicewareGenerator:
             last_index = index
             if len(accepted_words) == 6**4:
                 break
-    
+
         accepted_words.sort()
         indexgen = IndexGen([str(i + 1) for i in range(6)], 4)
         final_list = []
